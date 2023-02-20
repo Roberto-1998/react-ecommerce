7d809ffa34dcafaee4f32c86fa0073909518ec68
Roberto <rcastellong98@gmail.com>
2023-02-20 21:19:29 +0100

    [add] change to prod

--- src/pages/Cart/Cart.styled.jsx
+++ src/pages/Cart/Cart.styled.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { mobile } from "../../responsive";
 
 export const Container = styled.div`
-  height: ${(props) => (props.isEmpty === true ? "50vh" : "auto")};
+  height: ${(props) => (props.isEmpty === true ? "55vh" : "auto")};
 `;
 
 export const Wrapper = styled.div`
