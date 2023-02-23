b0d51b5a37524b7e82c0d84e90f3dd2578ed1322
Roberto <rcastellong98@gmail.com>
2023-02-23 21:23:23 +0100

    [add] ScrollToTop Component for every change of route

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import * as EN from "./locales/en";
 
 import i18next from "i18next";
 import { I18nextProvider } from "react-i18next";
+import { ScrollToTop } from "./helpers/ScrollToTop";
 
 i18next.init({
   interpolation: { escapeValue: false },
@@ -31,6 +32,7 @@ root.render(
       <PersistGate loading={null} persistor={persistor}>
         <Router>
           <I18nextProvider i18n={i18next}>
+            <ScrollToTop />
             <App />
           </I18nextProvider>
         </Router>
